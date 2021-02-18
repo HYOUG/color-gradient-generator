976c6dde4382d97309a603b2be47582825f8ea00
HYOUG <hyoug99931@gmail.com>
2021-02-18 21:47:03 +0100

    standardisation : turn the script into a function, examples and enhanced README

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Gradient Generator
-A simple python script that generate a color gradient
+A simple python script that generate a **color gradient**
 
-Usage :
-> python main.py (X size) (Y size) (R value start) (G value start) (B value start) (R value end) (G value end) (B value end)
+## ⚙️ Usage :
+
+> gen_gradient((X size, Y size), (R, G, B), (R, G, B), fp)
+
+## 📜 License
+
+MIT (2021) License protected project.
--- main.py
+++ main.py
@@ -1,31 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# script by "HYOUG"
+
 from PIL import Image
 from sys import argv
+from os import getcwd
 
-xy_size = tuple(argv[1:3])
-rgb_start = tuple(argv[3:6])
-rgb_end = tuple(argv[6:])
 
-xy_size = (int(xy_size[0]), int(xy_size[1]))
-rgb_start = (int(rgb_start[0]), int(rgb_start[1]), int(rgb_start[2]))
-rgb_end = (int(rgb_end[0]), int(rgb_end[1]), int(rgb_end[2]))
+def gen_gradient(xy_size: tuple = (1000, 1000), rgb_start: tuple = (0, 0, 0), rgb_end: tuple = (255, 255, 255), fp: str = f"{getcwd()}/gradient.png") -> None:
+    """Generate a gradient from the given format and the two RGB given"""
 
-R_GAP = (rgb_end[0] - rgb_start[0]) / xy_size[0]
-G_GAP = (rgb_end[1] - rgb_start[1]) / xy_size[0]
-B_GAP = (rgb_end[2] - rgb_start[2]) / xy_size[0]
+    xy_size = (int(xy_size[0]), int(xy_size[1]))
+    rgb_start = (int(rgb_start[0]), int(rgb_start[1]), int(rgb_start[2]))
+    rgb_end = (int(rgb_end[0]), int(rgb_end[1]), int(rgb_end[2]))
 
-im = Image.new("RGB", xy_size)
+    r_gap = (rgb_end[0] - rgb_start[0]) / xy_size[0]
+    g_gap = (rgb_end[1] - rgb_start[1]) / xy_size[0]
+    b_gap = (rgb_end[2] - rgb_start[2]) / xy_size[0]
 
-for x in range(xy_size[0]):
-    column_color = (int(rgb_start[0] + R_GAP * x),
-                    int(rgb_start[1] + G_GAP * x),
-                    int(rgb_start[2] + B_GAP * x))
-    for y in range(xy_size[1]):
-        im.putpixel((x, y), column_color)
+    im = Image.new("RGB", xy_size)
 
-im.save("gradient.png")
-im.close()
+    for x in range(xy_size[0]):
+        column_color = (int(rgb_start[0] + r_gap * x),
+                        int(rgb_start[1] + g_gap * x),
+                        int(rgb_start[2] + b_gap * x))
+        for y in range(xy_size[1]):
+            im.putpixel((x, y), column_color)
 
-print("The gradient have been saved under gradient.png in the working directory")
+    im.save(fp)
+    im.close()
+
 
 if __name__ == "__main__":
-    pass
+    xy_size = tuple(argv[1:3])
+    rgb_start = tuple(argv[3:6])
+    rgb_end = tuple(argv[6:])
+
+    gen_gradient(xy_size, rgb_start, rgb_end)
